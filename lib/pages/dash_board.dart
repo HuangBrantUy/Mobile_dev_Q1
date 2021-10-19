005a7c52676a178503e6b85e0d5ff8a69aa5046a
IncognitoForza <73091451+IncognitoForza@users.noreply.github.com>
2021-10-19 08:47:48 +0800

    Made changes to the dashboard

--- lib/pages/dash_board.dart
+++ lib/pages/dash_board.dart
@@ -13,34 +13,44 @@ class Dashboard extends StatelessWidget {
         title: const Text('Dashboard'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.8),
+        padding: const EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const TodoList()));
-                },
-                child: const Text('TO DO')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Profile()));
-                },
-                child: const Text('PROFILE')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SettingsPage()));
-                },
-                child: const Text('Settings'))
+            Text(
+              'Welcome to the To-do List app, Dear User',
+              style: TextStyle(fontSize: 18, height: 1.4),
+            ),
+            ElevatedButton.icon(
+              icon: Icon(Icons.list),
+              label: Text("To-Do List"),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const TodoList()));
+              },
+            ),
+            ElevatedButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("User Profile"),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Profile()));
+              },
+            ),
+            ElevatedButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text("Settings"),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingsPage()));
+              },
+            ),
           ],
         ),
       ),
     );
   }
 }
+
